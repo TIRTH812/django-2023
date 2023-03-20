4e0c820d2b2d3526c8a94bfec7757e6dbe99900c
TIRTH <tirth.webdev@gmail.com>
2023-03-20 15:14:35 +0530

    Email Functionality

--- user/views.py
+++ user/views.py
@@ -4,6 +4,10 @@ from .models import User
 from .forms import ManagerRegisterForm, DeveloperRegisterForm
 from django.contrib.auth import login
 from django.contrib.auth.views import LoginView
+from django.conf import settings
+#send mail
+from django.core.mail import send_mail
+from django.http import HttpResponse
 
 # Create your views here.
 class ManagerRegisterView(CreateView):
@@ -17,10 +21,17 @@ class ManagerRegisterView(CreateView):
     #     return super().get_context_data(**kwargs)
 
     def form_valid(self,form):
-        #email = form.cleaned_data.get('email')
-        user = form.save()
-        login(self.request,user)
-        return super().form_valid(form)
+        email = form.cleaned_data.get('email')
+        mail_response = sendMail(email)
+        # print(mail_response)
+        if mail_response > 0:
+            user = form.save()
+            login(self.request,user)
+            return super().form_valid(form)
+            # return HttpResponse('Mail Sent')
+        else:
+            print('Failed to send mail')
+            # return HttpResponse('Failed to send mail')
     
 class DeveloperRegisterView(CreateView):
     model = User
@@ -32,6 +43,19 @@ class DeveloperRegisterView(CreateView):
     #     kwargs['user_type'] = 'developer'
     #     return super().get_context_data(**kwargs)
 
+    def form_valid(self,form):
+        email = form.cleaned_data.get('email')
+        mail_response = sendMail(email)
+        # print(mail_response)
+        if mail_response > 0:
+            user = form.save()
+            login(self.request,user)
+            return super().form_valid(form)
+            # return HttpResponse('Mail Sent')
+        else:
+            print('Failed to send mail')
+            # return HttpResponse('Failed to send mail')
+
 class UserLoginView(LoginView):
     template_name = 'user/login.html'
     #success_url = "/"
@@ -40,5 +64,29 @@ class UserLoginView(LoginView):
         if self.request.user.is_authenticated:
             if self.request.user.is_manager1:
                 return '/cbv/list/'
-            else:
+            elif self.request.user.is_devleopr:
                 return '/developer/'
+            else:
+                return '/no_role/'
+            
+
+# def sendMail(request):
+#     subject = "welcome to django"
+#     message = "hello django"
+#     email_from = settings.EMAIL_HOST_USER
+#     recipient_list = ['tirthop812002@gmail.com']
+#     res = send_mail(subject,message,email_from,recipient_list)
+#     if res>0:
+#         print("mail sent")
+#     else:
+#         print("mail not sent")    
+#     print(res)
+#     return HttpResponse("mail sent")
+
+def sendMail(email):
+    subject = 'Welcome to the PG Finder'
+    message = 'We are glad to welcome you. Our Website helps you to find out best PG according to your requirements'
+    email_from = settings.EMAIL_HOST_USER
+    recipient_list = [email]
+    mail_response = send_mail(subject,message,email_from,recipient_list)
+    return mail_response
